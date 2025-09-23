188fb4c0b67770dcb703ca5f6532858a0a6b11aa
sembarface <114759842+sembarface@users.noreply.github.com>
2025-09-23 09:16:36 +0300

    Add files via upload

--- slau16.py
+++ slau16.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-f = open("pituhon/Chislaki/text.txt")
+f = open("text.txt")
 n = int(f.readline())
 A = []
 for _ in range(n):
@@ -10,28 +10,35 @@ f.close()
 
 A=np.array(A)
 b=np.array(b)
+eps=1e-10
+trans=np.transpose(A)
+sim=np.dot(trans,A)
+bsim=np.dot(trans,b)
+
+if np.array_equal(A,trans):
+    sim=A
+    bsim=b
 
 
 pred=np.array([0]*n)
+ksi=np.array([1]+[0]*(n-1))
+while(np.linalg.norm(ksi)> eps):
 
-for k in range(1,1000):
+    ksi=np.dot(sim,pred) -bsim
 
-    ksi=np.dot(A,pred) -b
-
-    tau=(np.dot(np.dot(A,ksi),ksi)/np.dot(np.dot(A,ksi),np.dot(A,ksi)))
+    tau=(np.dot(np.dot(sim,ksi),ksi)/np.dot(np.dot(sim,ksi),np.dot(sim,ksi)))
 
     x=pred-tau*ksi
     pred=x
+
+print("Ответ: ")
 print(x)
 
 '''
 for k in range(1,1000):
-    ksi=np.dot(A,pred) -b
-
-    tau=(np.dot(np.dot(A,ksi),ksi)/np.dot(np.dot(A,ksi),np.dot(A,ksi)))
-
+    ksi=np.dot(sim,pred) -bsim
+    tau=(np.dot(np.dot(sim,ksi),ksi)/np.dot(np.dot(sim,ksi),np.dot(sim,ksi)))
     E=np.eye(3)
-
     x=np.dot((E-tau*A),pred) + tau*b
     pred=x
 print(x)
@@ -44,4 +51,17 @@ print(x)
 2 1 4
 3 4 1
 14 16 14
+
+otvet 1 2 3
+'''
+
+
+'''
+3
+1 2 3
+0 3 5
+3 5 7
+-1 -3 -2
+
+otevet 1 -1 0
 '''
